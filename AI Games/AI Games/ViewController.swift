13b9f9dc9393dee1a27d897e34341a2d990f982d
tngok-wustl <n.tungluntony@wustl.edu>
2023-02-05 17:01:10 -0600

    Initial project

---
+++ AI Games/AI Games/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AI Games
+//
+//  Created by Tony Ngok on 05/02/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
