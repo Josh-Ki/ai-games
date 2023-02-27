8d5ceb83f258e94894866185ae7d6a65401cfca9
Josh-Ki <71088619+Josh-Ki@users.noreply.github.com>
2023-02-27 10:38:10 -0600

    Tic Tac Toe Remade

--- AI Games/AI Games/Games/TicTacToe/TicTacToeViewController.swift
+++ AI Games/AI Games/Games/TicTacToe/TicTacToeViewController.swift
@@ -21,6 +21,7 @@ class TicTacToeViewController: UIViewController {
     @IBOutlet weak var r3c3: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
+        initBoard()
     }
     
     var firstTurn = TicTacToeTurn.x
@@ -28,14 +29,75 @@ class TicTacToeViewController: UIViewController {
     
     var o = "O"
     var x = "X"
+    var oScore = 0
+    var xScore = 0
+    var board = [UIButton]()
     @IBAction func buttonTapped(_ sender: UIButton) {
+        
         addToBoard(sender)
+        if victory(x){
+            xScore += 1
+            resultAlert(title: "X WINS")
+        }
+        if victory(o){
+            oScore += 1
+            resultAlert(title: "O Wins")
+        }
+        if(boardIsFull()){
+            resultAlert(title: "Draw")
+            
+        }
+        
     }
-    
+    func initBoard() {
+        board.append(r1c1)
+        board.append(r1c2)
+        board.append(r1c3)
+        board.append(r2c1)
+        board.append(r2c2)
+        board.append(r2c3)
+        board.append(r3c1)
+        board.append(r3c2)
+        board.append(r3c3)
+    }
+    func resultAlert(title:String){
+        let ac = UIAlertController(title: title, message: nil, preferredStyle: .actionSheet)
+        ac.addAction(UIAlertAction(title: "Reset", style: .default, handler: { (_) in
+            self.resetBoard()
+        }))
+        self.present(ac, animated: true)
+    }
+    func resetBoard() {
+        for button in board {
+            button.setTitle(nil, for: .normal)
+            button.isEnabled = true
+        }
+        if firstTurn == TicTacToeTurn.o {
+            firstTurn = TicTacToeTurn.x
+            turnLabel.text = x
+        }
+        else if firstTurn == TicTacToeTurn.x {
+            firstTurn = TicTacToeTurn.o
+            turnLabel.text = o
+        }
+        currentTurn = firstTurn
+    }
+    func boardIsFull() -> Bool {
+        for button in board {
+            if button.title(for: .normal) == nil {
+                return false
+            }
+        }
+        return true
+    }
     func addToBoard(_ sender: UIButton){
         if(sender.title(for: .normal) == nil){
             if(currentTurn == TicTacToeTurn.o){
+                
                 sender.setTitle(o, for: .normal)
+
+                
+                
                 currentTurn = TicTacToeTurn.x
                 turnLabel.text = x
             }
@@ -44,7 +106,54 @@ class TicTacToeViewController: UIViewController {
                 currentTurn = TicTacToeTurn.o
                 turnLabel.text = o
             }
+            sender.isEnabled = false
         }
+    }
+    func victory(_ s :String) -> Bool
+        {
+            // Horizontal Victory
+            if thisSymbol(r1c1, s) && thisSymbol(r1c2, s) && thisSymbol(r1c3, s)
+            {
+                return true
+            }
+            if thisSymbol(r2c1, s) && thisSymbol(r2c2, s) && thisSymbol(r2c3, s)
+            {
+                return true
+            }
+            if thisSymbol(r3c1, s) && thisSymbol(r3c2, s) && thisSymbol(r3c3, s)
+            {
+                return true
+            }
+            
+            // Vertical Victory
+            if thisSymbol(r1c1, s) && thisSymbol(r2c1, s) && thisSymbol(r3c1, s)
+            {
+                return true
+            }
+            if thisSymbol(r1c2, s) && thisSymbol(r2c2, s) && thisSymbol(r3c2, s)
+            {
+                return true
+            }
+            if thisSymbol(r1c3, s) && thisSymbol(r2c3, s) && thisSymbol(r3c3, s)
+            {
+                return true
+            }
+            
+            // Diagonal Victory
+            if thisSymbol(r1c1, s) && thisSymbol(r2c2, s) && thisSymbol(r3c3, s)
+            {
+                return true
+            }
+            if thisSymbol(r1c3, s) && thisSymbol(r2c2, s) && thisSymbol(r3c1, s)
+            {
+                return true
+            }
+            
+            return false
+        }
+    
+    func thisSymbol(_ button: UIButton, _ symbol: String) -> Bool {
+        return button.title(for: .normal) == symbol
     }
     
     
