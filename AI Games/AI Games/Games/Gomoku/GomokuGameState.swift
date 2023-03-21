df7734e27f3f40c87f86c7a9b7eea87ddd8c6187
tngok-wustl <n.tungluntony@wustl.edu>
2023-03-21 00:19:09 -0500

    Gomoku interface (can play man VS man)

--- AI Games/AI Games/Games/Gomoku/GomokuGameState.swift
+++ AI Games/AI Games/Games/Gomoku/GomokuGameState.swift
@@ -14,6 +14,7 @@ struct GomokuGameState {
     let you: String
     var legalMoves: [Int] = []
     var state: Int = 20000
+    var winSeq: [Int] = []
     
     init(gameboard: [String], isBlack: Bool) {
         self.gameboard = gameboard
@@ -27,7 +28,13 @@ struct GomokuGameState {
         }
         
         self.legalMoves = getLegalMoves()
-        self.state = getState()
+        
+        let res = winLose()
+        self.state = getState(wl: res[0])
+        if ((self.state == -10000) || (self.state == 10000)) {
+            // https://www.marcosantadev.com/arrayslice-in-swift/
+            self.winSeq = Array(res[1...5]) // winning sequence
+        }
     }
     
     private func getLegalMoves() -> [Int] {
@@ -40,16 +47,17 @@ struct GomokuGameState {
         return y*10+x
     }
     
-    private func winLose() -> Int {
+    // return array containing utility score (first number) & winning sequence (other 5 numbers)
+    private func winLose() -> [Int] {
         // check horizontally
         for y in 0...9 {
             for x in 0...5 {
                 let pos = calcPos(y: y, x: x)
                 if ((gameboard[pos] == me) && (gameboard[pos+1] == me) && (gameboard[pos+2] == me) && (gameboard[pos+3] == me) && (gameboard[pos+4] == me)) {
-                    return 10000
+                    return [10000, pos, pos+1, pos+2, pos+3, pos+4]
                 }
                 if ((gameboard[pos] == you) && (gameboard[pos+1] == you) && (gameboard[pos+2] == you) && (gameboard[pos+3] == you) && (gameboard[pos+4] == you)) {
-                    return -10000
+                    return [-10000, pos, pos+1, pos+2, pos+3, pos+4]
                 }
             }
         }
@@ -59,10 +67,10 @@ struct GomokuGameState {
             for x in 0...9 {
                 let pos = calcPos(y: y, x: x)
                 if ((gameboard[pos] == me) && (gameboard[pos+10] == me) && (gameboard[pos+20] == me) && (gameboard[pos+30] == me) && (gameboard[pos+40] == me)) {
-                    return 10000
+                    return [10000, pos, pos+10, pos+20, pos+30, pos+40]
                 }
                 if ((gameboard[pos] == you) && (gameboard[pos+10] == you) && (gameboard[pos+20] == you) && (gameboard[pos+30] == you) && (gameboard[pos+40] == you)) {
-                    return -10000
+                    return [-10000, pos, pos+10, pos+20, pos+30, pos+40]
                 }
             }
         }
@@ -72,10 +80,10 @@ struct GomokuGameState {
             for x in 0...5 {
                 let pos = calcPos(y: y, x: x)
                 if ((gameboard[pos] == me) && (gameboard[pos+11] == me) && (gameboard[pos+22] == me) && (gameboard[pos+33] == me) && (gameboard[pos+44] == me)) {
-                    return 10000
+                    return [10000, pos, pos+11, pos+22, pos+33, pos+44]
                 }
                 if ((gameboard[pos] == you) && (gameboard[pos+11] == you) && (gameboard[pos+22] == you) && (gameboard[pos+33] == you) && (gameboard[pos+44] == you)) {
-                    return -10000
+                    return [-10000, pos, pos+11, pos+22, pos+33, pos+44]
                 }
             }
         }
@@ -85,24 +93,83 @@ struct GomokuGameState {
             for x in 0...5 {
                 let pos = calcPos(y: y, x: x)
                 if ((gameboard[pos] == me) && (gameboard[pos-9] == me) && (gameboard[pos-18] == me) && (gameboard[pos-27] == me) && (gameboard[pos-36] == me)) {
-                    return 10000
+                    return [10000, pos, pos-9, pos-18, pos-27, pos-36]
                 }
                 if ((gameboard[pos] == you) && (gameboard[pos-9] == you) && (gameboard[pos-18] == you) && (gameboard[pos-27] == you) && (gameboard[pos-36] == you)) {
-                    return -10000
+                    return [-10000, pos, pos-9, pos-18, pos-27, pos-36]
                 }
             }
         }
         
-        return 0
+        return [0]
     }
     
-    private func getState() -> Int {
-        let wl = winLose()
+    private func isDraw() -> Bool {
+        // game should end after at least 9 total moves from both players
+        if (legalMoves.count > 91) {
+            return false
+        }
+
+        // check horizontally
+        for y in 0...9 {
+            for x in 0...5 {
+                let pos = calcPos(y: y, x: x)
+                if (gameboard[pos] == me) {
+                    if ((gameboard[pos+1] != you) && (gameboard[pos+2] != you) && (gameboard[pos+3] != you) && (gameboard[pos+4] != you)) {
+                        return false
+                    }
+                }
+            }
+        }
+        
+        // check vertically
+        for y in 0...5 {
+            for x in 0...9 {
+                let pos = calcPos(y: y, x: x)
+                if (gameboard[pos] == me) {
+                    if ((gameboard[pos+10] != you) && (gameboard[pos+20] != you) && (gameboard[pos+30] != you) && (gameboard[pos+40] != you)) {
+                        return false
+                    }
+                }
+            }
+        }
+        
+        // check diagonally
+        for y in 0...5 {
+            for x in 0...5 {
+                let pos = calcPos(y: y, x: x)
+                if (gameboard[pos] == me) {
+                    if ((gameboard[pos+11] != you) && (gameboard[pos+22] != you) && (gameboard[pos+33] != you) && (gameboard[pos+44] != you)) {
+                        return false
+                    }
+                }
+            }
+        }
+        
+        // check reverse diagonally
+        for y in 4...9 {
+            for x in 0...5 {
+                let pos = calcPos(y: y, x: x)
+                if (gameboard[pos] == me) {
+                    if ((gameboard[pos-9] != you) && (gameboard[pos-18] != you) && (gameboard[pos-27] != you) && (gameboard[pos-36] != you)) {
+                        return false
+                    }
+                }
+            }
+        }
+        
+        return true
+    }
+    
+    private func getState(wl: Int) -> Int {
         if (wl != 0) {
             return wl
         }
         
         if ((wl == 0) && (legalMoves.isEmpty)) {
+            return 0
+        }
+        if (isDraw()) {
             return 0
         }
         
--- AI Games/AI Games/Games/Gomoku/GomokuViewController.swift
+++ AI Games/AI Games/Games/Gomoku/GomokuViewController.swift
@@ -7,23 +7,130 @@
 
 import UIKit
 
-class GomokuViewController: UIViewController {
-
+class GomokuViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    var gameboard: [String] = Array.init(repeating: "", count: 100)
+    var backColours: [UIColor] = []
+    var foreColours: [UIColor] = Array.init(repeating: UIColor.black, count: 100)
+    var turn = "B"
+    var gameOver = false
+    
+    // https://stackoverflow.com/questions/53768438/collectionview-cell-width-not-changing-for-different-nib
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 100
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! GomokuCell
+        
+        let i = indexPath.row
+        cell.pieceLabel.text = gameboard[i]
+        cell.backgroundColor = backColours[i]
+        cell.pieceLabel.textColor = foreColours[i]
+//        if (i % 20 < 10) {
+//            if (i % 2 == 0) {
+//                cell.backgroundColor = UIColor.yellow
+//            } else {
+//                cell.backgroundColor = UIColor.cyan
+//            }
+//        } else {
+//            if (i % 2 == 0) {
+//                cell.backgroundColor = UIColor.cyan
+//            } else {
+//                cell.backgroundColor = UIColor.yellow
+//            }
+//        }
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        if (!gameOver) {
+            let i = indexPath.row
+            if (gameboard[i] == "") {
+                gameboard[i] = turn
+                gomokuView.reloadItems(at: [indexPath])
+                
+                // switch turn
+                if (turn == "B") {
+                    turn = "W"
+                } else {
+                    turn = "B"
+                }
+                
+                let s = toGameState()
+                if (s.state == 20000) {
+                    turnLabel.text = "Turn: \(turn)"
+                } else {
+                    var msg = ""
+                    if (s.state == 10000) {
+                        msg = "\(bw(abbr: s.me)) WINS"
+                    } else if (s.state == -10000) {
+                        msg = "\(bw(abbr: s.you)) WINS"
+                    } else if (s.state == 0) {
+                        msg = "DRAW"
+                    }
+                    resultAlert(title: msg)
+                    turnLabel.text = msg
+                    
+                    // mark winning sequence on gameboard
+                    for i in s.winSeq {
+                        backColours[i] = UIColor.red
+                        foreColours[i] = UIColor.white
+                        
+                        // https://stackoverflow.com/questions/29428090/how-to-convert-int-to-nsindexpath
+                        gomokuView.reloadItems(at: [IndexPath(item: i, section: 0)])
+                    }
+                    
+                    gameOver = true
+                }
+            }
+        }
+    }
+    
+    @IBOutlet weak var gomokuView: UICollectionView!
+    @IBOutlet weak var turnLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        for i in 0...99 { // initialise colours of gameboard cells
+            if (i % 20 < 10) {
+                if (i % 2 == 0) {
+                    backColours.append(UIColor.yellow)
+                } else {
+                    backColours.append(UIColor.cyan)
+                }
+            } else {
+                if (i % 2 == 0) {
+                    backColours.append(UIColor.cyan)
+                } else {
+                    backColours.append(UIColor.yellow)
+                }
+            }
+        }
+        
+        gomokuView.delegate = self
+        gomokuView.dataSource = self
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func resultAlert(title: String) {
+        let ac = UIAlertController(title: title, message: nil, preferredStyle: .actionSheet)
+        ac.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        self.present(ac, animated: true)
     }
-    */
+    
+    func bw(abbr: String) -> String {
+        if (abbr == "W") {
+            return "WHITE"
+        }
+        return "BLACK"
+    }
+                            
+    func toGameState() -> GomokuGameState {
+        let isBlack = turn == "B" ? true : false
+        return GomokuGameState(gameboard: gameboard, isBlack: isBlack)
+    }
 
 }
